c02395a004a0229d1fc315daba3173864db3f15e
shaninalex <shanin.x@gmail.com>
2023-06-02 09:19:10 +0300

    change theme

--- lua/alexconfig/packer.lua
+++ lua/alexconfig/packer.lua
@@ -1,45 +1,46 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.1',
-        requires = {{
+        requires = { {
             'BurntSushi/ripgrep',
             'nvim-lua/plenary.nvim'
-        }}
+        } }
     }
 
     -- theme
+    -- 6. Base16 family
+    use { "rose-pine/neovim" }
     use { "ellisonleao/gruvbox.nvim" }
 
 
-    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     use('tpope/vim-fugitive')
     use {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v1.x',
         requires = {
             -- LSP Support
-            {'neovim/nvim-lspconfig'},             -- Required
-            {'williamboman/mason.nvim'},           -- Optional
-            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+            { 'neovim/nvim-lspconfig' },           -- Required
+            { 'williamboman/mason.nvim' },         -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
             -- Autocompletion
-            {'hrsh7th/nvim-cmp'},         -- Required
-            {'hrsh7th/cmp-nvim-lsp'},     -- Required
-            {'hrsh7th/cmp-buffer'},       -- Optional
-            {'hrsh7th/cmp-path'},         -- Optional
-            {'saadparwaiz1/cmp_luasnip'}, -- Optional
-            {'hrsh7th/cmp-nvim-lua'},     -- Optional
-            {'hrsh7th/nvim-gtd'},         -- Go
+            { 'hrsh7th/nvim-cmp' },       -- Required
+            { 'hrsh7th/cmp-nvim-lsp' },   -- Required
+            { 'hrsh7th/cmp-buffer' },     -- Optional
+            { 'hrsh7th/cmp-path' },       -- Optional
+            { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+            { 'hrsh7th/cmp-nvim-lua' },   -- Optional
+            { 'hrsh7th/nvim-gtd' },       -- Go
 
             -- Snippets
-            {'L3MON4D3/LuaSnip'},             -- Required
-            {'rafamadriz/friendly-snippets'}, -- Optional
+            { 'L3MON4D3/LuaSnip' },           -- Required
+            { 'rafamadriz/friendly-snippets' }, -- Optional
         }
     }
 
@@ -52,7 +53,7 @@ return require('packer').startup(function(use)
     use 'neovim/nvim-lspconfig'
 
     -- toggle term
-    use {'akinsho/toggleterm.nvim', tag = '*' }
+    use { 'akinsho/toggleterm.nvim', tag = '*' }
 
     use {
         "windwp/nvim-autopairs",
@@ -63,6 +64,4 @@ return require('packer').startup(function(use)
     use "kdheepak/lazygit.nvim"
 
     use 'kkvh/vim-docker-tools'
-
 end)
-
