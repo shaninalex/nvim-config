d224881d261f08019c77a7b232687a57e5fe1a1c
shaninalex <shanin.x@gmail.com>
2023-09-22 08:15:23 +0300

    back to gruvbox

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,6 +1,24 @@
-require("catppuccin").setup({
-    transparent_background = false,
-    no_italic = true
+require("gruvbox").setup({
+    undercurl = true,
+    underline = true,
+    bold = true,
+    italic = {
+        strings = false,
+        operators = false,
+        comments = false,
+        folds = false,
+    },
+    strikethrough = true,
+    invert_selection = false,
+    invert_signs = false,
+    invert_tabline = false,
+    invert_intend_guides = false,
+    inverse = true, -- invert background for search, diffs, statuslines and errors
+    contrast = "hard", -- can be "hard", "soft" or empty string
+    palette_overrides = {},
+    overrides = {},
+    dim_inactive = false,
+    transparent_mode = true,
 })
-vim.cmd([[colorscheme catppuccin-mocha]])
-
+vim.o.background = "dark"
+vim.cmd([[colorscheme gruvbox]])
