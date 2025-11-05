2879217685d5e5f54b39728d6ef572e5e96d88fc
shaninalex <shanin.x@gmail.com>
2025-11-05 07:50:10 +0200

    go debugger

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -17,17 +17,61 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	{
-		"neanias/everforest-nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {
-			background = "hard",
-			italics = false,
-			disable_italic_comments = true,
-			ui_contrast = true,
+		"navarasu/onedark.nvim",
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			require("onedark").setup({
+				style = "darker",
+			})
+			-- Enable theme
+			require("onedark").load()
+		end,
+	},
+
+	-- Nvim dap go
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"leoluz/nvim-dap-go",
+			"rcarriga/nvim-dap-ui",
+			"theHamsta/nvim-dap-virtual-text",
+			"nvim-neotest/nvim-nio",
+			"williamboman/mason.nvim",
 		},
 		config = function()
-			vim.cmd("colorscheme everforest")
+			local dap = require("dap")
+			local ui = require("dapui")
+
+			require("dapui").setup()
+			require("dap-go").setup()
+
+			vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
+			vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
+
+			-- Eval var under cursor
+			vim.keymap.set("n", "<space>?", function()
+				require("dapui").eval(nil, { enter = true })
+			end)
+
+			vim.keymap.set("n", "<F1>", dap.continue)
+			vim.keymap.set("n", "<F2>", dap.step_into)
+			vim.keymap.set("n", "<F3>", dap.step_over)
+			vim.keymap.set("n", "<F4>", dap.step_out)
+			vim.keymap.set("n", "<F5>", dap.step_back)
+			vim.keymap.set("n", "<F6>", dap.restart)
+
+			dap.listeners.before.attach.dapui_config = function()
+				ui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				ui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				ui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				ui.close()
+			end
 		end,
 	},
 
